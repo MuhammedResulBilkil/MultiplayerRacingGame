014f3605c1a057b5d56736ea14fa790039953961
Resul <garnnet_221@hotmail.com>
2022-03-18 02:05:48 +0300

    Racing Game Mode: Car Movement Script

---
+++ Assets/Scripts/RacingMode/CarMovement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+namespace RacingMode
+{
+    public class CarMovement : MonoBehaviour
+    {
+        private Rigidbody _rigidbody;
+        
+        private Vector3 _thrustForce = new Vector3(0f, 0f, 45f);
+        private Vector3 _rotationTorque = new Vector3(0f, 8f, 0f);
+
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey("w"))
+            {
+                _rigidbody.AddRelativeForce(_thrustForce);
+            }
+            
+            if (Input.GetKey("s"))
+            {
+                _rigidbody.AddRelativeForce(-_thrustForce);
+            }
+            
+            if (Input.GetKey("a"))
+            {
+                _rigidbody.AddRelativeTorque(-_rotationTorque);
+            }
+            
+            if (Input.GetKey("d"))
+            {
+                _rigidbody.AddRelativeTorque(_rotationTorque);
+            }
+        }
+    }
+}
