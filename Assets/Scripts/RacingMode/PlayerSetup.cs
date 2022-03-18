5c707b67b683aa6f1bfcfaf4ad394c52ee11b0a7
Resul <garnnet_221@hotmail.com>
2022-03-18 11:55:50 +0300

    Racing Game Mode:
     PlayerSetup Script And Synchronizing Movement Across The Network

---
+++ Assets/Scripts/RacingMode/PlayerSetup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Photon.Pun;
+using UnityEngine;
+
+namespace RacingMode
+{
+    public class PlayerSetup : MonoBehaviourPunCallbacks
+    {
+        [SerializeField] private Camera _mainCamera;
+
+        private CarMovement _carMovement;
+
+        private void Awake()
+        {
+            _carMovement = GetComponent<CarMovement>();
+        }
+
+        private void Start()
+        {
+            if (photonView.IsMine)
+            {
+                _carMovement.enabled = true;
+                _mainCamera.enabled = true;
+            }
+            else
+            {
+                _carMovement.enabled = false;
+                _mainCamera.enabled = false;
+            }
+        }
+    }
+}
