9cf4f836b6fceeee655a085bc3268e1367c8f8e8
Resul <garnnet_221@hotmail.com>
2022-03-20 23:46:30 +0300

    Death Race Game Mode: Shooting With Bullets

---
+++ Assets/Scripts/DeathRaceMode/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace DeathRaceMode
+{
+    public class Bullet : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Destroy(gameObject);
+        }
+
+        public void Initializer(Vector3 direction, float speed, float damage)
+        {
+            transform.forward = direction;
+
+            _rigidbody.velocity = direction * speed;
+        }
+    }
+}
---
+++ Assets/Scripts/DeathRaceMode/Shooting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace DeathRaceMode
+{
+    public class Shooting : MonoBehaviour
+    {
+        [SerializeField] private GameObject _bulletPrefab;
+        [SerializeField] private Transform _firePosition;
+        [SerializeField] private Camera _camera;
+        [SerializeField] private DeathRacePlayer _deathRacePlayerProperties;
+
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Fire();
+            }
+        }
+
+        private void Fire()
+        {
+            Ray ray = _camera.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+            
+            GameObject bulletGameObject = Instantiate(_bulletPrefab, _firePosition.position, Quaternion.identity);
+            bulletGameObject.GetComponent<Bullet>().Initializer(ray.direction, _deathRacePlayerProperties.bulletSpeed,
+                _deathRacePlayerProperties.damage);
+        }
+    }
+}
