f74d7406e0f567239879ef9cef1708f1717eff3d
Resul <garnnet_221@hotmail.com>
2022-03-20 15:04:43 +0300

    Scriptable Objects created for all Game Modes!

---
+++ Assets/Scripts/DeathRaceMode/DeathRacePlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace DeathRaceMode
+{
+    [CreateAssetMenu(menuName = "Death Race Mode/Create Death Race Player", fileName = "New Death Race Player")]
+    public class DeathRacePlayer : ScriptableObject
+    {
+        public string playerName;
+        public Sprite playerSprite;
+
+        [Header("Weapon Properties")] 
+        public string weaponName;
+        public float damage;
+        public float fireRate;
+        public float bulletSpeed;
+    }
+}
---
+++ Assets/Scripts/RacingMode/RacingPlayer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace RacingMode
+{
+    [CreateAssetMenu(fileName = "New Racing Player", menuName = "Racing Mode/Create Racing Player", order = 0)]
+    public class RacingPlayer : ScriptableObject
+    {
+        public string playerName;
+        public Sprite playerSprite;
+    }
+}
